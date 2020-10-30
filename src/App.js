ece426378973c15261bad0d3c712dfc35bc92062
= <=>
2020-10-30 08:30:52 +0400

    Components imported
    - ChatList
    
    - Chat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import "./App.css";
+import ChatList from "./components/ChatList/ChatList";
+import Chat from "./components/Chat/Chat";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <ChatList />
+      <Chat />
+    </div>
+  );
 }
 
 export default App;
